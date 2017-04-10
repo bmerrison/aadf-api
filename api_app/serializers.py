75a0e4fec4c7c50efaae07150a44f3ee21968332
Bob <bmerrison@gmail.com>
2017-04-10 21:22:22 +0100

    Count point and traffic count endpoints now allow you to specify criteria for filtering.

--- api_app/serializers.py
+++ api_app/serializers.py
@@ -50,19 +50,43 @@ class RoadSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'category', 'category_code' )
 
 class CountPointSerializer(serializers.ModelSerializer):
+    road_name = serializers.ReadOnlyField(source='road.name')
+    road_category_code = serializers.ReadOnlyField(source='road.category.code')
+    local_authority_name = serializers.ReadOnlyField(source='local_authority.name')
+    region_name = serializers.ReadOnlyField(source='local_authority.region.name')
+    start_junction_desc = serializers.ReadOnlyField(source='start_junction.description')
+    end_junction_desc = serializers.ReadOnlyField(source='end_junction.description')
+    link_length_miles = serializers.SerializerMethodField('km_to_miles')
+    
+    def km_to_miles(self, instance):
+        return round(instance.link_length / 1.60934, 1)
     
     class Meta:
         model = CountPoint
-        fields = ('id', 'reference', 'local_authority', 'road', 'easting', 'northing',
-                  'start_junction', 'end_junction', 'link_length')
+        fields = ('id', 'reference', 'local_authority', 'local_authority_name',
+                  'region_name', 'road', 'road_name', 'road_category_code', 'easting',
+                  'northing', 'start_junction', 'start_junction_desc', 'end_junction',
+                  'end_junction_desc', 'link_length', 'link_length_miles')
         
 class TrafficCountSerializer(serializers.ModelSerializer):
+    count_point_ref = serializers.ReadOnlyField(source='count_point.reference')
+    estimation_details = serializers.ReadOnlyField(source='estimation_method.description')
+    count_hgv = serializers.SerializerMethodField('get_hgv_count')
+    count_all = serializers.SerializerMethodField('get_all_count')
 
+    def get_hgv_count(self, tc):
+        return tc.count_hgv_2ax_rigid + tc.count_hgv_3ax_rigid + tc.count_hgv_45ax_rigid + \
+            tc.count_hgv_34ax_artic + tc.count_hgv_5ax_artic + tc.count_hgv_6plus_artic
+
+    def get_all_count(self, tc):
+        return self.get_hgv_count(tc) + tc.count_cycles + tc.count_motorcycles + \
+            tc.count_cars + tc.count_buses + tc.count_lightgoods
+    
     class Meta:
         model = TrafficCount
-        fields = ('id', 'count_point', 'year', 'estimated', 'estimation_method',
+        fields = ('id', 'count_point', 'count_point_ref', 'year', 'estimated', 'estimation_method', 'estimation_details',
                   'count_cycles', 'count_motorcycles', 'count_cars',
                   'count_buses', 'count_lightgoods', 'count_hgv_2ax_rigid',
                   'count_hgv_3ax_rigid', 'count_hgv_45ax_rigid',
                   'count_hgv_34ax_artic', 'count_hgv_5ax_artic',
-                  'count_hgv_6plus_artic')
+                  'count_hgv_6plus_artic', 'count_hgv', 'count_all')
--- api_app/views.py
+++ api_app/views.py
@@ -46,8 +46,50 @@ class CountPointViewSet(viewsets.ModelViewSet):
     queryset = CountPoint.objects.all()
     serializer_class = CountPointSerializer    
 
+    def get_queryset(self):
+        queryset = CountPoint.objects.all()
+
+        # Optional simple filter criteria. If multiple criteria are given,
+        # the results must match them all.
+        possible_filters = (('reference', 'reference'),
+                            ('road_name', 'road__name'),)
+        kwargs = {}
+        for req_param, attrib in possible_filters:
+            req_val = self.request.query_params.get(req_param, None)
+            if req_val is not None:
+                kwargs[attrib] = req_val
+        queryset = queryset.filter(**kwargs)
+
+        # Optional criteria that can be used to retrieve count points in
+        # a given rectangle.
+        if 'min_northing' in self.request.query_params:
+            queryset = queryset.filter(northing__gte=self.request.query_params['min_northing'])
+        if 'max_northing' in self.request.query_params:
+            queryset = queryset.filter(northing__lte=self.request.query_params['max_northing'])
+        if 'min_easting' in self.request.query_params:
+            queryset = queryset.filter(easting__gte=self.request.query_params['min_easting'])
+        if 'max_easting' in self.request.query_params:
+            queryset = queryset.filter(easting__lte=self.request.query_params['max_easting'])
+            
+        return queryset
+    
 class TrafficCountViewSet(viewsets.ModelViewSet):
-    queryset = TrafficCount.objects.all()
     serializer_class = TrafficCountSerializer    
     
-    
+    def get_queryset(self):
+        queryset = TrafficCount.objects.all()
+
+        # Optional simple filter criteria. If multiple criteria are given,
+        # the results must match them all.
+        possible_filters = (('count_point_ref', 'count_point__reference'),
+                            ('year', 'year'),
+                            ('estimated', 'estimated'),
+                            ('road_name', 'count_point__road__name'),)
+        kwargs = {}
+        for req_param, attrib in possible_filters:
+            req_val = self.request.query_params.get(req_param, None)
+            if req_val is not None:
+                kwargs[attrib] = req_val
+        queryset = queryset.filter(**kwargs)
+
+        return queryset
