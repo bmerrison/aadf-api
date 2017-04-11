97f919a61579977e147e3f2bb788d640ecc14940
Bob <bmerrison@gmail.com>
2017-04-11 21:09:47 +0100

    Updated settings to use AWS database when running on EB.

--- aadf_api/settings.py
+++ aadf_api/settings.py
@@ -74,21 +74,32 @@ WSGI_APPLICATION = 'aadf_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'aadf',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres', # Don't get excited, just for my dev PC.
-        'HOST': 'localhost',
-        'PORT': '',
-    },    
-    'sqlite': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'aadf',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres', # Don't get excited, just for my dev PC.
+            'HOST': 'localhost',
+            'PORT': '',
+        },    
+        'sqlite': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
